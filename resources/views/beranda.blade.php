4ced3a30286fa48cc072684c47aeee0dbb746962
nanda443 <nandanurikhsan443@gmail.com>
2023-10-31 21:13:34 +0700

    12211856-menyelesaikan pembuatan template tampilan dan routing

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,13 @@
+@extends('template')
+
+@section('content')
+<div class="pagetitle">
+    <h1>Beranda</h1>
+</div>
+  <!-- End Page Title -->
+@endsection
+
+@section('main')
+{{-- ketikkan kode disini --}}
+
+@endsection

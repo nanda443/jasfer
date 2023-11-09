669d3515cf1fb19e8c4a2c8b0b4cb3ca9563bb29
nanda443 <nandanurikhsan443@gmail.com>
2023-11-10 01:51:51 +0700

    fixing halaman kamera dan fotografer

--- resources/views/fotografer.blade.php
+++ resources/views/fotografer.blade.php
@@ -12,8 +12,8 @@
 <div class="row m-0 row-cols-1 row-cols-md-3 g-4">
     <div class="col">
       <div class="card h-100">
-        <img src="{{ asset('template/assets/img/fotografer/ade.jpg') }}" class="card-img-top rounded" alt="...">
-        <div class="card-body">
+        <img src="{{ asset('template/assets/img/fotografer/ade.jpg') }}" class="card-img-top w-75 mx-auto mt-3 rounded-circle " alt="...">
+        <div class="card-body pb-0">
           <h5 class="card-title fw-bold pb-0 fs-5">Ade Eko ardiansyah</h5>
           <h6 class="card-subtitle mb-2 text-muted">Jogja Potret</h6>
           <p class="card-text mb-0">berpengalaman selama 5 tahun. Melayani acara wisuda, wedding, liburan dll.</p>
@@ -27,8 +27,8 @@
     </div>
     <div class="col">
         <div class="card h-100">
-          <img src="{{ asset('template/assets/img/fotografer/zikri.jpg') }}" class="card-img-top rounded" alt="...">
-          <div class="card-body">
+          <img src="{{ asset('template/assets/img/fotografer/zikri.jpg') }}" class="card-img-top w-75 mx-auto mt-3 rounded-circle " alt="...">
+          <div class="card-body pb-0">
             <h5 class="card-title fw-bold pb-0 fs-5">Ahmad Zikri</h5>
             <h6 class="card-subtitle mb-2 text-muted">Jogja Potret</h6>
             <p class="card-text mb-0">berpengalaman selama 5 tahun. Melayani acara wisuda, wedding, liburan dll.</p>
@@ -42,8 +42,8 @@
     </div>
     <div class="col">
         <div class="card h-100">
-          <img src="{{ asset('template/assets/img/fotografer/kemon.jpg') }}" class="card-img-top rounded" alt="...">
-          <div class="card-body">
+          <img src="{{ asset('template/assets/img/fotografer/kemon.jpg') }}" class="card-img-top w-75 mx-auto mt-3 rounded-circle " alt="...">
+          <div class="card-body pb-0">
             <h5 class="card-title fw-bold pb-0 fs-5">ALi Imron</h5>
             <h6 class="card-subtitle mb-2 text-muted">Jogja Potret</h6>
             <p class="card-text mb-0">berpengalaman selama 5 tahun. Melayani acara wisuda, wedding, liburan dll.</p>
@@ -57,8 +57,8 @@
     </div>
     <div class="col">
         <div class="card h-100">
-          <img src="{{ asset('template/assets/img/fotografer/rakha.jpeg') }}" class="card-img-top rounded" alt="...">
-          <div class="card-body">
+          <img src="{{ asset('template/assets/img/fotografer/rakha.jpeg') }}" class="card-img-top w-75 mx-auto mt-3 rounded-circle " alt="...">
+          <div class="card-body pb-0">
             <h5 class="card-title fw-bold pb-0 fs-5">Rakha Aryadi Pratama</h5>
             <h6 class="card-subtitle mb-2 text-muted">Nanda Studio</h6>
             <p class="card-text mb-0">Percayakan moment berhargamu pada kami!</p>
@@ -72,8 +72,8 @@
     </div>
     <div class="col">
         <div class="card h-100">
-          <img src="{{ asset('template/assets/img/fotografer/gupong.jpg') }}" class="card-img-top rounded" alt="...">
-          <div class="card-body">
+          <img src="{{ asset('template/assets/img/fotografer/gupong.jpg') }}" class="card-img-top w-75 mx-auto mt-3 rounded-circle " alt="...">
+          <div class="card-body pb-0">
             <h5 class="card-title fw-bold pb-0 fs-5">Kemon Putra Utama</h5>
             <h6 class="card-subtitle mb-2 text-muted">Nanda Studio</h6>
             <p class="card-text mb-0">Percayakan moment berhargamu pada kami!</p>
@@ -87,8 +87,8 @@
     </div>
     <div class="col">
         <div class="card h-100">
-          <img src="{{ asset('template/assets/img/fotografer/lukman.jpg') }}" class="card-img-top rounded" alt="...">
-          <div class="card-body">
+          <img src="{{ asset('template/assets/img/fotografer/lukman.jpg') }}" class="card-img-top w-75 mx-auto mt-3 rounded-circle " alt="...">
+          <div class="card-body pb-0">
             <h5 class="card-title fw-bold pb-0 fs-5">Eko Budi Santoso</h5>
             <h6 class="card-subtitle mb-2 text-muted">Eko Potography</h6>
             <p class="card-text mb-0">Jangan sia-siakan moment berharga dalam hidupmu! Pesan sekarang!</p>
@@ -102,8 +102,8 @@
     </div>
     <div class="col">
         <div class="card h-100">
-          <img src="{{ asset('template/assets/img/messages-3.jpg') }}" class="card-img-top rounded" alt="...">
-          <div class="card-body">
+          <img src="{{ asset('template/assets/img/messages-3.jpg') }}" class="card-img-top w-75 mx-auto mt-3 rounded-circle " alt="...">
+          <div class="card-body pb-0">
             <h5 class="card-title fw-bold pb-0 fs-5">Hasan Junior</h5>
             <h6 class="card-subtitle mb-2 text-muted">Eko Potography</h6>
             <p class="card-text mb-0">Jangan sia-siakan moment berharga dalam hidupmu! Pesan sekarang!</p>
--- resources/views/kamera.blade.php
+++ resources/views/kamera.blade.php
@@ -10,8 +10,8 @@
 <div class="row m-0 row-cols-1 row-cols-md-3 g-4">
     <div class="col">
       <div class="card h-100">
-        <img src="{{ asset('template/assets/img/kamera/sonya7.jpg') }}" class="card-img-top rounded" alt="...">
-        <div class="card-body">
+        <img src="{{ asset('template/assets/img/kamera/sonya7.jpg') }}" class="card-img-top rounded w-75 mx-auto " alt="...">
+        <div class="card-body pb-0">
           <h5 class="card-title fw-bold pb-0 fs-5">Sony A7</h5>
           <h6 class="card-subtitle mb-2 text-muted">Jogja Potret</h6>
           <p class="card-text mb-0">Sony A7 menjadi Kamera Sony Full Frame Terbaik keluaran pertama yang dirilis oleh Sony.</p>
@@ -25,11 +25,11 @@
     </div>
     <div class="col">
         <div class="card h-100">
-          <img src="{{ asset('template/assets/img/kamera/sonya6000.jpg') }}" class="card-img-top rounded" alt="...">
-          <div class="card-body">
+          <img src="{{ asset('template/assets/img/kamera/sonya6000.jpg') }}" class="card-img-top rounded w-75 mx-auto" alt="...">
+          <div class="card-body pb-0">
             <h5 class="card-title fw-bold pb-0 fs-5">Sony A6000</h5>
             <h6 class="card-subtitle mb-2 text-muted">Jogja Potret</h6>
-            <p class="card-text mb-0">Kamera yang dikenal dapat menghasilkan gambar dengan resolusi tinggi di dalam kondisi cahaya minim ini sangat disukai banyak orang.</p>
+            <p class="card-text mb-0">Kamera yang dikenal dapat menghasilkan gambar dengan resolusi tinggi.</p>
             <p class="card-text"><small class="text-muted"><a href="">Detail</a></small></p>
           </div>
           <div class="card-footer">
@@ -40,8 +40,8 @@
     </div>
     <div class="col">
         <div class="card h-100">
-          <img src="{{ asset('template/assets/img/kamera/m6.jpg') }}" class="card-img-top rounded" alt="...">
-          <div class="card-body">
+          <img src="{{ asset('template/assets/img/kamera/m6.jpg') }}" class="card-img-top rounded w-75 mx-auto" alt="...">
+          <div class="card-body pb-0">
             <h5 class="card-title fw-bold pb-0 fs-5">Canon Eos M50</h5>
             <h6 class="card-subtitle mb-2 text-muted">Jogja Potret</h6>
             <p class="card-text mb-0">berpengalaman selama 5 tahun. Melayani acara wisuda, wedding, liburan dll.</p>
@@ -55,8 +55,8 @@
     </div>
     <div class="col">
         <div class="card h-100">
-          <img src="{{ asset('template/assets/img/kamera/m6.jpg') }}" class="card-img-top rounded" alt="...">
-          <div class="card-body">
+          <img src="{{ asset('template/assets/img/kamera/m6.jpg') }}" class="card-img-top rounded w-75 mx-auto" alt="...">
+          <div class="card-body pb-0">
             <h5 class="card-title fw-bold pb-0 fs-5">Canon Eos M6</h5>
             <h6 class="card-subtitle mb-2 text-muted">Nanda Studio</h6>
             <p class="card-text mb-0">Percayakan moment berhargamu pada kami!</p>
@@ -70,8 +70,8 @@
     </div>
     <div class="col">
         <div class="card h-100">
-          <img src="{{ asset('template/assets/img/kamera/r6.jpg') }}" class="card-img-top rounded" alt="...">
-          <div class="card-body">
+          <img src="{{ asset('template/assets/img/kamera/r6.jpg') }}" class="card-img-top rounded w-75 mx-auto" alt="...">
+          <div class="card-body pb-0">
             <h5 class="card-title fw-bold pb-0 fs-5">Canon Eos R6</h5>
             <h6 class="card-subtitle mb-2 text-muted">Nanda Studio</h6>
             <p class="card-text mb-0">Percayakan moment berhargamu pada kami!</p>
@@ -85,8 +85,8 @@
     </div>
     <div class="col">
         <div class="card h-100">
-          <img src="{{ asset('template/assets/img/kamera/sonya7.jpg') }}" class="card-img-top rounded" alt="...">
-          <div class="card-body">
+          <img src="{{ asset('template/assets/img/kamera/sonya7.jpg') }}" class="card-img-top rounded w-75 mx-auto" alt="...">
+          <div class="card-body pb-0">
             <h5 class="card-title fw-bold pb-0 fs-5">Sony A7</h5>
             <h6 class="card-subtitle mb-2 text-muted">Eko Potography</h6>
             <p class="card-text mb-0">Jangan sia-siakan moment berharga dalam hidupmu! Pesan sekarang!</p>
@@ -100,8 +100,8 @@
     </div>
     <div class="col">
         <div class="card h-100">
-          <img src="{{ asset('template/assets/img/kamera/sonya6000.jpg') }}" class="card-img-top rounded" alt="...">
-          <div class="card-body">
+          <img src="{{ asset('template/assets/img/kamera/sonya6000.jpg') }}" class="card-img-top rounded w-75 mx-auto" alt="...">
+          <div class="card-body pb-0">
             <h5 class="card-title fw-bold pb-0 fs-5">Sony A6000</h5>
             <h6 class="card-subtitle mb-2 text-muted">Eko Potography</h6>
             <p class="card-text mb-0">Jangan sia-siakan moment berharga dalam hidupmu! Pesan sekarang!</p>

e7b1934b660cc6d624473e4dbbd0fa0c3e7f0181
nanda443 <nandanurikhsan443@gmail.com>
2023-11-10 00:18:56 +0700

    12211856_memperbaiki halaman kamera

--- resources/views/kamera.blade.php
+++ resources/views/kamera.blade.php
@@ -1,97 +1,117 @@
 @extends('template')
 
 @section('content')
-<div class="pagetitle">
+<div class="pagetitle mb-0">
     <h1>Kamera</h1>
 </div>
 @endsection
 
 @section('main')
-<div class="row row-cols-1 row-cols-md-3 g-4">
+<div class="row m-0 row-cols-1 row-cols-md-3 g-4">
     <div class="col">
-        <div class="card mb-4 h-100">
-            <img src="{{ asset('template/assets/img/sonya6000.jpg') }}" class="card-img-top center" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Sony a6000</h5>
-                <p class="card-text">Rp.300.000/Hari</p>
-                <p class="card-text">Nanda Kamera</p>
-                <div class="position-absolute bottom-0 end-0 p-3">
-                    <a href="#" class="btn btn-primary">Booking Now</a>
-                    <a href="#" class="card-link">Detail</a>
-                </div>
-            </div>
+      <div class="card h-100">
+        <img src="{{ asset('template/assets/img/kamera/sonya7.jpg') }}" class="card-img-top rounded" alt="...">
+        <div class="card-body">
+          <h5 class="card-title fw-bold pb-0 fs-5">Sony A7</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Jogja Potret</h6>
+          <p class="card-text mb-0">Sony A7 menjadi Kamera Sony Full Frame Terbaik keluaran pertama yang dirilis oleh Sony.</p>
+          <p class="card-text"><small class="text-muted"><a href="">Detail</a></small></p>
+        </div>
+        <div class="card-footer">
+          <p class="card-text text-danger fw-bolder float-end">Rp. 200.000/hari</p>
+          <button type="button" class="btn btn-primary w-100"><i class="bi bi-cart-check m-2"></i><span>pesan Sekarang</span></button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+        <div class="card h-100">
+          <img src="{{ asset('template/assets/img/kamera/sonya6000.jpg') }}" class="card-img-top rounded" alt="...">
+          <div class="card-body">
+            <h5 class="card-title fw-bold pb-0 fs-5">Sony A6000</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Jogja Potret</h6>
+            <p class="card-text mb-0">Kamera yang dikenal dapat menghasilkan gambar dengan resolusi tinggi di dalam kondisi cahaya minim ini sangat disukai banyak orang.</p>
+            <p class="card-text"><small class="text-muted"><a href="">Detail</a></small></p>
+          </div>
+          <div class="card-footer">
+            <p class="card-text text-danger fw-bolder float-end">Rp. 300.000/hari</p>
+            <button type="button" class="btn btn-primary w-100"><i class="bi bi-cart-check m-2"></i><span>pesan Sekarang</span></button>
+          </div>
         </div>
     </div>
     <div class="col">
-        <div class="card mb-4 h-100">
-            <img src="{{ asset('template/assets/img/sonya7.jpg') }}" class="card-img-top center" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Sony a7</h5>
-                <p class="card-text">Rp.500.000/Hari</p>
-                <p class="card-text">Rakha Kamera</p>
-                <div class="position-absolute bottom-0 end-0 p-3">
-                    <a href="#" class="btn btn-primary">Booking Now</a>
-                    <a href="#" class="card-link">Detail</a>
-                </div>
-            </div>
+        <div class="card h-100">
+          <img src="{{ asset('template/assets/img/kamera/m6.jpg') }}" class="card-img-top rounded" alt="...">
+          <div class="card-body">
+            <h5 class="card-title fw-bold pb-0 fs-5">Canon Eos M50</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Jogja Potret</h6>
+            <p class="card-text mb-0">berpengalaman selama 5 tahun. Melayani acara wisuda, wedding, liburan dll.</p>
+            <p class="card-text"><small class="text-muted"><a href="">Detail</a></small></p>
+          </div>
+          <div class="card-footer">
+            <p class="card-text text-danger fw-bolder float-end ">Rp. 150.000/hari</p>
+            <button type="button" class="btn btn-primary w-100"><i class="bi bi-cart-check m-2"></i><span>pesan Sekarang</span></button>
+          </div>
         </div>
     </div>
     <div class="col">
-        <div class="card mb-4 h-100">
-            <img src="{{ asset('template/assets/img/m50.jpg') }}" class="card-img-top center" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Canon Eos M50</h5>
-                <p class="card-text">Rp.350.000/Hari</p>
-                <p class="card-text">Ade Kamera</p>
-                <div class="position-absolute bottom-0 end-0 p-3">
-                    <a href="#" class="btn btn-primary">Booking Now</a>
-                    <a href="#" class="card-link">Detail</a>
-                </div>
-            </div>
+        <div class="card h-100">
+          <img src="{{ asset('template/assets/img/kamera/m6.jpg') }}" class="card-img-top rounded" alt="...">
+          <div class="card-body">
+            <h5 class="card-title fw-bold pb-0 fs-5">Canon Eos M6</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Nanda Studio</h6>
+            <p class="card-text mb-0">Percayakan moment berhargamu pada kami!</p>
+            <p class="card-text"><small class="text-muted"><a href="">Detail</a></small></p>
+          </div>
+          <div class="card-footer">
+            <p class="card-text text-danger fw-bolder float-end ">Rp. 200.000/hari</p>
+            <button type="button" class="btn btn-primary w-100"><i class="bi bi-cart-check m-2"></i><span>pesan Sekarang</span></button>
+          </div>
         </div>
     </div>
     <div class="col">
-        <div class="card mb-4 h-100">
-            <img src="{{ asset('template/assets/img/rp.jpg') }}" class="card-img-top center" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Canon Eos RP</h5>
-                <p class="card-text">Rp.500.000/Hari</p>
-                <p class="card-text">Nanda Kamera</p>
-                <div class="position-absolute bottom-0 end-0 p-3">
-                    <a href="#" class="btn btn-primary">Booking Now</a>
-                    <a href="#" class="card-link">Detail</a>
-                </div>
-            </div>
+        <div class="card h-100">
+          <img src="{{ asset('template/assets/img/kamera/r6.jpg') }}" class="card-img-top rounded" alt="...">
+          <div class="card-body">
+            <h5 class="card-title fw-bold pb-0 fs-5">Canon Eos R6</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Nanda Studio</h6>
+            <p class="card-text mb-0">Percayakan moment berhargamu pada kami!</p>
+            <p class="card-text"><small class="text-muted"><a href="">Detail</a></small></p>
+          </div>
+          <div class="card-footer">
+            <p class="card-text text-danger fw-bolder float-end ">Rp. 400.000/hari</p>
+            <button type="button" class="btn btn-primary w-100"><i class="bi bi-cart-check m-2"></i><span>pesan Sekarang</span></button>
+          </div>
         </div>
     </div>
     <div class="col">
-        <div class="card mb-4 h-100">
-            <img src="{{ asset('template/assets/img/m6.jpg') }}" class="card-img-top center" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Canon Eos M6</h5>
-                <p class="card-text">Rp.250.000/Hari</p>
-                <p class="card-text">Rakha Kamera</p>
-                <div class="position-absolute bottom-0 end-0 p-3">
-                    <a href="#" class="btn btn-primary">Booking Now</a>
-                    <a href="#" class="card-link">Detail</a>
-                </div>
-            </div>
+        <div class="card h-100">
+          <img src="{{ asset('template/assets/img/kamera/sonya7.jpg') }}" class="card-img-top rounded" alt="...">
+          <div class="card-body">
+            <h5 class="card-title fw-bold pb-0 fs-5">Sony A7</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Eko Potography</h6>
+            <p class="card-text mb-0">Jangan sia-siakan moment berharga dalam hidupmu! Pesan sekarang!</p>
+            <p class="card-text"><small class="text-muted"><a href="">Detail</a></small></p>
+          </div>
+          <div class="card-footer">
+            <p class="card-text text-danger fw-bolder float-end ">Rp. 100.000/hari</p>
+            <button type="button" class="btn btn-primary w-100"><i class="bi bi-cart-check m-2"></i><span>pesan Sekarang</span></button>
+          </div>
         </div>
     </div>
     <div class="col">
-        <div class="card mb-4 h-100">
-            <img src="{{ asset('template/assets/img/r6.jpg') }}" class="card-img-top center" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Canon Eos R6</h5>
-                <p class="card-text">Rp.550.000/Hari</p>
-                <p class="card-text">Ade Kamera</p>
-                <div class="position-absolute bottom-0 end-0 p-3">
-                    <a href="#" class="btn btn-primary">Booking Now</a>
-                    <a href="#" class="card-link">Detail</a>
-                </div>
-            </div>
+        <div class="card h-100">
+          <img src="{{ asset('template/assets/img/kamera/sonya6000.jpg') }}" class="card-img-top rounded" alt="...">
+          <div class="card-body">
+            <h5 class="card-title fw-bold pb-0 fs-5">Sony A6000</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Eko Potography</h6>
+            <p class="card-text mb-0">Jangan sia-siakan moment berharga dalam hidupmu! Pesan sekarang!</p>
+            <p class="card-text"><small class="text-muted"><a href="">Detail</a></small></p>
+          </div>
+          <div class="card-footer">
+            <p class="card-text text-danger fw-bolder float-end ">Rp. 100.000/hari</p>
+            <button type="button" class="btn btn-primary w-100"><i class="bi bi-cart-check m-2"></i><span>pesan Sekarang</span></button>
+          </div>
         </div>
     </div>
 </div>
-
 @endsection
